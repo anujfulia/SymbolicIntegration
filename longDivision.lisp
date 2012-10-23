cc15a461d96bd554ecd2b36145ecef4bc1245112
karthikabinav <karthikabinavs@gmail.com>
2012-10-24 01:00:21 +0530

    Added some comments

--- longDivision.lisp
+++ longDivision.lisp
@@ -1,6 +1,10 @@
+;given a polynomial of form ("+" (coeff variable power) (coeff variable power) ..)
+;returns (maxPower coeffofhigestpower coeffofnexthighest ...)
+;Sample input (getCoeff '("+" (4 x 3)) 0)
+;Sample output (3 4 0 0 0)
 (defun getCoeff (lst power)
     (cond
-        ((equal (length lst) 0) nil)
+        ((equal power 0) 0)
         ((string= (first lst) "+") (cons (third (second lst)) (getCoeff (cdr lst) (third (second lst)))  ))
         ((equal power (third (first lst))) (cons (first (car lst)) (getCoeff (cdr lst) (- power 1))))
         (t (cons 0  (getCoeff (cdr lst) (- power 1))))
