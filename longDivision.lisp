feb277b2cdc9fe9162296a324cc3c9bd2e94065d
karthikabinav <karthikabinavs@gmail.com>
2012-10-24 15:34:09 +0530

    Added the get polynomial from coefficients function

--- longDivision.lisp
+++ longDivision.lisp
@@ -1,14 +1,18 @@
+;A general assumption on input. The polynomial shoule be written in decresaing order of power. i.e. 3x + 4x^3 should be input as '("+" (4 x 3) (3 x 1))
+
 ;given a polynomial of form ("+" (coeff variable power) (coeff variable power) ..)
 ;returns (maxPower coeffofhigestpower coeffofnexthighest ...)
-;Sample input (getCoeff '("+" (4 x 3)) 0)
+;Sample input (getCoeff '("+" (4 x 3)) -1)
 ;Sample output (3 4 0 0 0)
+;NOTE: Applicable only for integer powers
+;Initially call with power = -1
 (defun getCoeff (lst power)
     (if (stringp (first lst))
         (cond
             ((string= (first lst) "+") (cons (third (second lst)) (getCoeff (cdr lst) (third (second lst)))  ))
         )
         (cond
-            ((= power -1) nil)
+            ((= (length lst) 0) nil)
             ((= power (third (first lst))) (cons (car (car lst)) (getCoeff (cdr lst) (- power 1))))
             (t (cons 0 (getCoeff lst (- power 1))))
         )
@@ -16,7 +20,23 @@
 )
 
 ; Get polynomial from a coefficients
+; Note status says if its the first time called or not
+; Initially call with power = -1
+; Will return a polynomial in x
+(defun getPoly (coeff status power)
+   (if (= status 1)
+        (
+         cons "+" (getPoly (cdr coeff) 0 (car coeff))
+        )
+       (if (= (length coeff) 0) nil
+        (cons
+         (list (first coeff) "x" power)
+         (getPoly (cdr coeff) 0 (- power 1))
 
+        )
+       )
+    )
+)
 
 
 (defun test(lst)
@@ -52,6 +72,6 @@
  
 
     (setf numExp (getCoeff numerator 0))
-    (setf denExp (getCoeffdenominator 0))
+    (setf denExp (getCoeff denominator 0))
  
 )
